541c37143d6cdb0539d2bab8d541d4cdc8bfa9e4
Tong Lin <wowawawoa@gmail.com>
2023-12-11 11:55:17 +1030

    basic layout

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<?php 
+
+    while (have_posts()) {
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <hr>
+    <?php }
+
+?>
+
+<?php get_footer(); ?>
